aa5977a5d0a4cc1597604e0ff7cb7f711ed4e1b6
Xiang Zhang <xiang.z.zhang@gmail.com>
2015-12-16 19:36:01 +0800

    1.  1st solution for Add_Digit_258

--- Add_Digit_258.py
+++ Add_Digit_258.py
@@ -1,1 +1,24 @@
 __author__ = 'xizhang'
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        sum = num
+        while sum >= 10:
+            sum = self.get_sum(sum)
+        return sum
+
+    def get_sum(self, num):
+        sum = 0
+        while num >= 10:
+            sum += num % 10
+            num = int(num/10)
+        sum += num
+        return sum
+
+if __name__ == '__main__':
+    solu = Solution()
+    print solu.addDigits(38)
+    print solu.addDigits(10)
