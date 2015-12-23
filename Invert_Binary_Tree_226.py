75ca49226a3b650fbd6c9b5c7ac115f36585fa5b
xiang.zhang <xiang.z.zhang@gmail.com>
2015-12-23 22:59:46 +0800

    1. add issue's LeetCode link

--- Invert_Binary_Tree_226.py
+++ Invert_Binary_Tree_226.py
@@ -1,11 +1,12 @@
 __author__ = 'amsun'
+"https://leetcode.com/problems/invert-binary-tree/"
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-"https://leetcode.com/problems/invert-binary-tree/"
 class Solution(object):
     def invertTree(self, root):
         """
