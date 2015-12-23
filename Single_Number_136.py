3a102c6f4e27023c96b2f4c722a082b01f71fb38
Xiang Zhang <xiang.z.zhang@gmail.com>
2015-12-23 19:40:42 +0800

    1.  1st version of single number but time exceeded

--- Single_Number_136.py
+++ Single_Number_136.py
@@ -7,31 +7,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # nums = []
         if len(nums) == 1:
             return nums[0]
-
-        i = 0
-        j = 1
-        while len(nums) > 1:
-            if nums[i] == nums[j]:
-                nums.pop(j)
-                nums.pop(i)
-                if j >= len(nums) - 1:
-                    j = len(nums) - 1
-                    i += 1
-
-                if i == 0:
-                    j += 1
-            else:
-                if j >= len(nums) - 1:
-                    j = len(nums) - 1
-                    i += 1
-
-                if i == 0:
-                    j += 1
+        else:
+            while len(nums) > 1 and nums[0] in nums[1:]:
+                nums.pop(0)
+            return nums[0]
 
         return nums[0]
 if __name__ == '__main__':
     solu = Solution()
-    print solu.singleNumber([4,3,5,6,7,5,4,7,6])
-    print solu.singleNumber([1,0,1])
+    print solu.singleNumber([4, 3, 5, 6, 7, 5, 4, 7, 6])
+    print solu.singleNumber([1, 0, 1])
