5166d0a70b3b1a917e972df4cb9152798d3610a9
xiang.zhang <xiang.z.zhang@gmail.com>
2020-03-17 23:17:57 +0800

    solution for https://leetcode.com/problems/implement-strstr/

--- Algorithm/Python/implement-strstr.py
+++ Algorithm/Python/implement-strstr.py
@@ -4,4 +4,23 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        pass
+        if not haystack:
+            return -1
+
+        for index, char in enumerate(haystack):
+            if needle[0] == char:
+                for sub_index, i in enumerate(needle[1:]):
+                    if index + sub_index + 1 < len(haystack):
+                        if i != haystack[index + sub_index + 1]:
+                            break
+                    else:
+                        return -1
+                else:
+                    return index
+
+        return -1
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.strStr("aaaa", "aaaaa"))
+    print(solution.strStr("Hello", "bc"))
