0df7a93229fe6d73dd77b1d0745c0644838b6fcb
xiang.zhang <xiang.z.zhang@gmail.com>
2020-03-22 23:18:42 +0800

    TODO for https://leetcode.com/problems/maximum-subarray/

---
+++ Algorithm/Python/maximum-subarray-53.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/maximum-subarray/
+
+
+class Solution:
+    def maxSubArray(self, nums: list) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
