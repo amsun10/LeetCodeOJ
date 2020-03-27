3a982818d5e288a0c65322699a543957dcebaeab
xiang.zhang <xiang.z.zhang@gmail.com>
2020-03-28 00:21:02 +0800

    solution for https://leetcode.com/problems/maximum-subarray/

--- Algorithm/Python/maximum-subarray-53.py
+++ Algorithm/Python/maximum-subarray-53.py
@@ -3,26 +3,24 @@
 
 class Solution:
     def maxSubArray(self, nums: list) -> int:
-        result = nums[0]
+        max_sum = nums[0]
         temp_sum = nums[0]
-        start_index = 0
         for index, i in enumerate(nums[1:]):
-            temp_sum = result + i
-            if temp_sum >= result:
-                if temp_sum < i:
-                    result = i
-                else:
-                    result = temp_sum
-
-
-
-
-
-
+            temp_sum += i
+            if temp_sum < i:
+                if max_sum < i:
+                    max_sum = i
+                temp_sum = i
+            else:
+                if temp_sum > max_sum:
+                    max_sum = temp_sum
         return max_sum
-        pass
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+    print(solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(solution.maxSubArray([-2, -1]))
+    print(solution.maxSubArray([-1, -2]))
+
+    print(solution.maxSubArray([8, -19, 5, -4, 20]))
