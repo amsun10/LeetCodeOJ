dbe9443ef20b5efd0b6233dddf8eaf07ff419c78
xiang.zhang <xiang.z.zhang@gmail.com>
2020-04-02 22:58:34 +0800

    solution for https://leetcode.com/problems/minimum-depth-of-binary-tree/

--- Algorithm/Python/minimum-depth-of-binary-tree-111.py
+++ Algorithm/Python/minimum-depth-of-binary-tree-111.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -13,31 +14,17 @@ class Solution:
         min_depth = 0
 
         if root:
-            min_depth = 1
-            if root.left and root.right:
-               min_depth += min(self.tree_height(root.left), self.tree_height(root.right))
-            elif root.left:
-                min_depth += self.tree_height(root.left)
-            else:
-                min_depth += self.tree_height(root.right)
+            min_depth += 1
+        else:
+            return 0
+
+        if root.left is None:
+            return min_depth + self.minDepth(root.right)
+
+        if root.right is None:
+            return min_depth + self.minDepth(root.left)
+
+        min_depth += min(self.minDepth(root.left), self.minDepth(root.right))
 
         return min_depth
-
-    def tree_height(self, node: TreeNode):
-        height = 0
-
-        if node:
-            height += 1
-            left_height = 0
-            right_height = 0
-            if node.left:
-                left_height = self.tree_height(node.left)
-
-            if node.right:
-                right_height = self.tree_height(node.right)
-
-
-            height += min(left_height, right_height)
-
-        return height
 
