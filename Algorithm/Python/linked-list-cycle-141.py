a6042835fdc6b6c621ff9f7bad37b63b11be8d4c
xiang.zhang <xiang.z.zhang@gmail.com>
2021-01-17 23:29:15 +0800

    solution updated for https://leetcode.com/problems/linked-list-cycle/

--- Algorithm/Python/linked-list-cycle-141.py
+++ Algorithm/Python/linked-list-cycle-141.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/linked-list-cycle/
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -17,4 +18,21 @@ class Solution:
             else:
                 address_list.append(id(head.next))
             head = head.next
+        return False
+
+
+class Solution2:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+
+        slow = head
+        fast = head
+
+        while slow is not None and fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
         return False
